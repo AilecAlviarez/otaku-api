b57ffd19b836f0d94345d693d4865d52a5d95a92
maria de los angeles <mariaramireeez2002@gmail.com>
2020-10-10 16:39:02 -0400

    add Tymon/jwt-auth

--- config/app.php
+++ config/app.php
@@ -174,6 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        //Providers app
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
     ],
 
